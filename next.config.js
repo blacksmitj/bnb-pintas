5613d4d5bfa7241ac717153dce2b0d7a2e651117
blacksmitj <andri.andreas.panjaitan@gmail.com>
2023-05-19 03:52:29 +0700

    adding bnb pintas domains image

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
     domains: [
       "avatars.githubusercontent.com",
       "lh3.googleusercontent.com",
-      "res.cloudinary.com"
+      "res.cloudinary.com",
+      "bnb-pintas.vercel.app"
     ]
   }
 }
